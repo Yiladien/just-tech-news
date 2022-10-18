c534fe90fa8cf887bc31d87828a3f4eead901b73
yiladien@hotmail.com <yiladien@hotmail.com>
2022-10-18 00:57:53 -0500

    connection updates

--- models/index.js
+++ models/index.js
@@ -1,111 +1,60 @@
-<<<<<<< HEAD
-const User = require("./User");
+// import all models
 const Post = require("./Post");
+const User = require("./User");
 const Vote = require("./Vote");
 const Comment = require("./Comment");
-=======
-// import all models
-const Post = require('./Post');
-const User = require('./User');
-const Vote = require('./Vote');
-const Comment = require('./Comment');
->>>>>>> efed14c5cf184e19aa2da168ff2489717a3c3223
 
 // create associations
 User.hasMany(Post, {
-  foreignKey: 'user_id'
-});
-
-Post.belongsTo(User, {
-  foreignKey: 'user_id'
-});
-
-<<<<<<< HEAD
-Vote.belongsTo(User, {
   foreignKey: "user_id",
 });
 
-Vote.belongsTo(Post, {
-  foreignKey: "post_id",
+Post.belongsTo(User, {
+  foreignKey: "user_id",
 });
 
 User.belongsToMany(Post, {
   through: Vote,
   as: "voted_posts",
   foreignKey: "user_id",
-=======
-User.belongsToMany(Post, {
-  through: Vote,
-  as: 'voted_posts',
-  foreignKey: 'user_id'
->>>>>>> efed14c5cf184e19aa2da168ff2489717a3c3223
 });
 
 Post.belongsToMany(User, {
   through: Vote,
-<<<<<<< HEAD
   as: "voted_posts",
   foreignKey: "post_id",
 });
 
-User.hasMany(Vote, {
-  foreignKey: "user_id",
-});
-
-Post.hasMany(Vote, {
-  foreignKey: "post_id",
-});
-
-Comment.belongsTo(User, {
-  foreignKey: "user_id",
-});
-
-Comment.belongsTo(Post, {
-  foreignKey: "post_id",
-});
-
-User.hasMany(Comment, {
-  foreignKey: "user_id",
-});
-
-Post.hasMany(Comment, {
-  foreignKey: "post_id",
-=======
-  as: 'voted_posts',
-  foreignKey: 'post_id'
-});
-
 Vote.belongsTo(User, {
-  foreignKey: 'user_id'
+  foreignKey: "user_id",
 });
 
 Vote.belongsTo(Post, {
-  foreignKey: 'post_id'
+  foreignKey: "post_id",
 });
 
 User.hasMany(Vote, {
-  foreignKey: 'user_id'
+  foreignKey: "user_id",
 });
 
 Post.hasMany(Vote, {
-  foreignKey: 'post_id'
+  foreignKey: "post_id",
 });
 
 Comment.belongsTo(User, {
-  foreignKey: 'user_id'
+  foreignKey: "user_id",
 });
 
 Comment.belongsTo(Post, {
-  foreignKey: 'post_id'
+  foreignKey: "post_id",
 });
 
 User.hasMany(Comment, {
-  foreignKey: 'user_id'
+  foreignKey: "user_id",
 });
 
 Post.hasMany(Comment, {
-  foreignKey: 'post_id'
->>>>>>> efed14c5cf184e19aa2da168ff2489717a3c3223
+  foreignKey: "post_id",
 });
 
 module.exports = { User, Post, Vote, Comment };
